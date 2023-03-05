6666c9c1c784a2d705ec9c70ba6ea6b735cff122
Gregory Maselle <110554679+OratorZA@users.noreply.github.com>
2023-03-05 14:34:10 +0200

    Almost Final
    Large files can be sent across both ways with working hash functions and validation. Missing comments and potential concurrency.

--- Assignment1New/Client.py
+++ Assignment1New/Client.py
@@ -13,43 +13,75 @@ if (IP == "123"):
 def goFetch(fileName, clientSocket):
   print("gofetch run\n")
   done = False
-  hashCodePlusByte = clientSocket.recv(1024)
-  hashCode = hashCodePlusByte[12:hashCodePlusByte.index(b'<2><SFILSEN>')].decode()
-  print(hashCodePlusByte)
-  print(hashCode)
-  fileBytes = b""
-  #   byteData = clientSocket.recv(1024)[12:]
-  byteData = hashCodePlusByte[hashCodePlusByte.index(b'<2><SFILSEN>')+12:]
-  print(byteData)
-  while not done:
-    #byteData = clientSocket.recv(1024)
-    #byteData = fileBytes
-    if byteData[-5:] == b"<END>":
-      print("Test")
-      done = True
-      fileBytes += byteData[:-5]
-      print(byteData)
-      print(fileBytes)
-    else:
-      #byteData = clientSocket.recv(1024)
-      fileBytes += byteData
-      byteData = clientSocket.recv(1024)
-      
-      
-  print("TEST")
-  print(os.getcwd())
-  os.chdir("./clientDownloads")
   hValidation = hashlib.sha256()
-  hValidation.update(fileBytes)
-  #hValidation.hexdigest()
-  print(hValidation.hexdigest() == hashCode)
-  if(hValidation.hexdigest() != hashCode):
-    print("FATAL ERROR: FILE MISMATCH")
+  os.chdir("./clientDownloads")
+  with open(fileName, "wb") as f:
+    while True:
+      data = clientSocket.recv(4096)
+
+      if data == b"<END>":
+       # print("\nDONE\n")
+        break
+
+      f.write(data)
+      hValidation.update(data)
+      #print(b"WRITING DATA "+data)
+      clientSocket.send("Data recieved".encode(MSGFORMAT))
+  hashCode = clientSocket.recv(1024).decode()[12:]
+  if(hValidation.hexdigest() == "hashCode"):
+    clientSocket.send("<0><VLDDATA>The hash codes are equal - file is being downloaded.".encode(MSGFORMAT))
+  #   file = open(fileName, "wb")
+  #   file.write(fileBytes)
+  #   file.close()
+    print("File is downloaded - please reconnect if you would like to download/upload any more files.")
     clientSocket.close()
     exit(1)
-  file = open(fileName, "wb")
-  file.write(fileBytes)
-  file.close()
+  else:
+    clientSocket.send("<0><INVDATA>The hash codes do not match. Please retransmit the data.".encode(MSGFORMAT))
+  #   goFetch(fileName, clientSocket)
+    print("Data received is invalid, please reconnect and try again.")
+    os.remove(fileName)
+    clientSocket.close()
+    exit(1)
+
+  
+  # hashCodePlusByte = clientSocket.recv(1024)
+  # hashCode = hashCodePlusByte[12:hashCodePlusByte.index(b'<2><SFILSEN>')].decode()
+  # print(hashCodePlusByte)
+  # print(hashCode)
+  # fileBytes = b""
+  # #   byteData = clientSocket.recv(1024)[12:]
+  # byteData = hashCodePlusByte[hashCodePlusByte.index(b'<2><SFILSEN>')+12:]
+  # print(byteData)
+  # while not done:
+  #   #byteData = clientSocket.recv(1024)
+  #   #byteData = fileBytes
+  #   if byteData[-5:] == b"<END>":
+  #     print("Test")
+  #     done = True
+  #     fileBytes += byteData[:-5]
+  #     print(byteData)
+  #     print(fileBytes)
+  #   else:
+  #     #byteData = clientSocket.recv(1024)
+  #     fileBytes += byteData
+  #     byteData = clientSocket.recv(1024)
+      
+      
+  # print("TEST")
+  # print(os.getcwd())
+  # os.chdir("./clientDownloads")
+  # hValidation = hashlib.sha256()
+  # hValidation.update(fileBytes)
+  # #hValidation.hexdigest()
+  # print(hValidation.hexdigest() == hashCode)
+  # if(hValidation.hexdigest() != hashCode):
+  #   print("FATAL ERROR: FILE MISMATCH")
+  #   clientSocket.close()
+  #   exit(1)
+  # file = open(fileName, "wb")
+  # file.write(fileBytes)
+  # file.close()
 
 
 
@@ -118,23 +150,51 @@ def fileSend(clientSocket):
       clientSocket.send(("<2><PASSEND>"+password).encode(MSGFORMAT))
       print(clientSocket.recv(1024).decode()[12:0])
   
-  file = open(fileName, 'rb')
-    #filesize = os.path.getsize(reqFile)
-    #connectionSocket.send(str(filesize).encode())
-  data = file.read()
   hValidation = hashlib.sha256()
-  hValidation.update(data)
-  clientSocket.sendall(("<2><HEXSEND>" + hValidation.hexdigest()).encode(MSGFORMAT))
-  #print(data)
-  # print(type(data))
-  clientSocket.sendall(b"<2><SFILSEN>"+data+b"<END>")
-  #connectionSocket.send(b"<END>")
-  file.close()
+  with open(fileName, "rb") as f:
+      while True:
+          data = f.read(4096)
+ 
+          if not data:
+              break
+ 
+          clientSocket.sendall(data)
+          hValidation.update(data)
+          #print("sending still")
+          msg = clientSocket.recv(1024).decode(MSGFORMAT)
+      clientSocket.send(b"<END>")
+  clientSocket.send(("<2><HEXVALU>"+hValidation.hexdigest()).encode(MSGFORMAT))
+  response = clientSocket.recv(1024).decode()
+  if(response[3:12] == "INVDATA"):
+      print("File did not send successfully - hash codes not equal.")
+      clientSocket.close()
+      os.chdir("../")
+      return
+  else:
+      os.chdir("../")
+      print("File sent successfully.")
+      clientSocket.close()
+      return    
+  # file = open(fileName, 'rb')
+  #   #filesize = os.path.getsize(reqFile)
+  #   #connectionSocket.send(str(filesize).encode())
+  # data = file.read()
+  # hValidation = hashlib.sha256()
+  # hValidation.update(data)
+  # clientSocket.sendall(("<2><HEXSEND>" + hValidation.hexdigest()).encode(MSGFORMAT))
+  # #print(data)
+  # # print(type(data))
+  # clientSocket.sendall(b"<2><SFILSEN>"+data+b"<END>")
+  # #connectionSocket.send(b"<END>")
+  # file.close()
 
 
 def main():
   clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  clientSocket.connect((IP, serverPort))
+  try:
+    clientSocket.connect((IP, serverPort))
+  except socket.error as e:
+      print(str(e))
   prompt = clientSocket.recv(1024)
   print('From Server: ', prompt.decode()[12:])
   inp = input('')
--- Assignment1New/Server.py
+++ Assignment1New/Server.py
@@ -3,82 +3,114 @@ import os
 import hashlib
 import sys
 
+
 MSGFORMAT = "utf-8"
 serverPort = 9999
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP = socket.gethostbyname(socket.gethostname())#gets server IP
+IP = socket.gethostbyname(socket.gethostname())# gets server IP address which is used to create the server socket for listening to connections.
 print(IP)
-serverSocket.bind((IP, serverPort))
+# this try catch is used to catch any errors that occur with the socket being used to listen for connections.
+try:
+    serverSocket.bind((IP, serverPort))
+except socket.error as e:
+    print(str(e))
 
-def findAndSend(reqFile,connectionSocket):
+
+def findAndSend(reqFile,connectionSocket):#This method is run when a client tries to download a file from the server
     #passfail = 3;
     #print("Hello World")
-    os.chdir('./Database')
-    pwords = open('passwords.txt','r')
+    os.chdir('./Database') # enters the database directory where the files available for downloading by the client are stored.
+    pwords = open('passwords.txt','r') # opens the passwords file used to check whether a requested file name exists in the database and whether it requires a password/ what the password is.
     valid = False
     while not valid:
         for line in pwords.readlines():
-            lineList = line.split('/')   # Assuming we store as [filename]/[open or not]/[password]/[attempts]
+            lineList = line.split('/')   #Used to split each line of the passwords file into its file name, privacy and password.
             #for item in lineList:
                 #print(item)
             target = lineList[0]
-            print(target)
+            #print(target)
             pword = lineList[2]
-            print(pword)
-            if target == reqFile:
-                valid = True
-                if (lineList[1] == "open"):
+            #print(pword)
+            if target == reqFile:#if the filename is located in the passwords file
+                valid = True # checks 
+                if (lineList[1] == "open"):#The file downloads if the file is not password protected
+                    # if the file is opened, the user is informed of this and the server moves to start sending the file.
                     print(lineList[1])
                     connectionSocket.send("<0><NONPASS>NOPASSREQUIRED".encode(MSGFORMAT))
-                    sendFile(reqFile, connectionSocket)
-                else:
-                    passFail = 3
+                    sendFile(reqFile, connectionSocket)#runs method to send the file to the client
+                else:#if there is a password for the file
+                    passFail = 3#the client has 3 tries to input the password
                     passreq = "<0><PREQUES>Please input password for "+reqFile
-                    connectionSocket.send(passreq.encode(MSGFORMAT))
+                    connectionSocket.send(passreq.encode(MSGFORMAT))#prompt to input password sent
                     while (passFail > 0):
-                        passSent = connectionSocket.recv(1024).decode()[12:]
-                        print("\nHELLO THIS IS THE PASS SENT"+passSent)
+                        passSent = connectionSocket.recv(1024).decode()[12:]#input received from client
+                        #print("\nHELLO THIS IS THE PASS SENT"+passSent)
                     
-                        if (str(passSent) == (pword)):
+                        if (str(passSent) == (pword)):#when the correct password is input
                             #print("Test correct")
                             successMessage = "<0><PACCEPT>Correct Password. Sending "+reqFile+"\n"
-                            connectionSocket.send(successMessage.encode(MSGFORMAT))
-                            sendFile(reqFile,connectionSocket)
+                            connectionSocket.send(successMessage.encode(MSGFORMAT))#notifies client that password is correct
+                            sendFile(reqFile,connectionSocket)#sends file to client
                             break
                             
                         else:
                             #failureMessage = "<PREJECT>Password incorrect. You have " + str(passFail-1) + " attempts left.\n"
-                            failureMessage = "<0><PREJECT>Password incorrect. You have " + str(passFail-1)+" attempts left.\n"
+                            failureMessage = "<0><PREJECT>Password incorrect. You have " + str(passFail-1)+" attempts left.\n"#an error message is sent after an incorrect password attempt
                             
                             passFail = passFail - 1
                             connectionSocket.send(failureMessage.encode(MSGFORMAT))
                     print(str(passFail)+"PASSFAIL")
                 # print(passfail == 0)
-                    if (passFail == 0):
-                        exitMessage = "You have exceeded the amount of attempts allowed to enter the password.\n"
+                    if (passFail == 0):#once the client fails the password input 3 times
+                        exitMessage = "You have exceeded the amount of attempts allowed to enter the password.\n"#an error message is sent
                         print(exitMessage)
                         connectionSocket.send(("<1><EXCPASS>"+exitMessage).encode(MSGFORMAT))
-                        connectionSocket.close()
-        if (valid == False):
+                        connectionSocket.close()#and the socket is closed
+        if (valid == False):#check in case the filename is not found in passwords.txt
             connectionSocket.send("<0><INVNAME>INVALIDNAME".encode(MSGFORMAT))
             print(reqFile + "THIS IS REQFILE")
             reqFile = connectionSocket.recv(1024).decode()[12:]
+
             pwords.seek(0)
                     
 
-def sendFile(reqFile,connectionSocket): # Sends filesize, bytes with a tagasd
-    print("SENDING FILE")
-    file = open(reqFile, 'rb')
-    data = file.read()
+def sendFile(reqFile,connectionSocket): # Sends filesize, bytes with a tag
+    #print("SENDING FILE")
+    #file = open(reqFile, 'rb')
+   # data = file.read()
     hValidation = hashlib.sha256()
-    hValidation.update(data)
+    with open(reqFile, "rb") as f:
+        while True:
+            data = f.read(4096)
+ 
+            if not data:
+                break
+ 
+            connectionSocket.sendall(data)
+            hValidation.update(data)
+            #print("sending still")
+            msg = connectionSocket.recv(1024).decode(MSGFORMAT)
+        connectionSocket.send(b"<END>")
+    connectionSocket.send(("<2><HEXVALU>"+hValidation.hexdigest()).encode(MSGFORMAT))
+    response = connectionSocket.recv(1024).decode()
+    if(response[3:12] == "INVDATA"):
+        print("File did not send successfully - hash codes not equal.")
+        connectionSocket.close()
+        os.chdir("../")
+        return
+    else:
+        os.chdir("../")
+        print("File sent successfully.")
+        connectionSocket.close()
+        return        
+    
     #hValidation.digest()
     #print(hValidation.hexdigest())
    #     connectionSocket.sendall(("<2><HEXSEND>" + hValidation.hexdigest()).encode(MSGFORMAT))
    #     print(b"\n"+data)
    #     connectionSocket.sendall(b"<2><SFILSEN>"+data+b"<END>")
-    connectionSocket.send(("<2><HEXSEND>"+hValidation.hexdigest()).encode(MSGFORMAT)+b"<2><SFILSEN>"+data+b"<END>")
-    file.close()
+    #   connectionSocket.sendall(("<2><HEXSEND>"+hValidation.hexdigest()).encode(MSGFORMAT)+b"<2><SFILSEN>"+data+b"<END>")
+    #file.close()
     os.chdir("../")
     print("Successful")
 
@@ -125,41 +157,55 @@ def recvFile(connectionSocket):
         file = open("passwords.txt", 'a')
         file.write(fName + "/" + priv + "/" + pWord + "/" + "\n")
 
-    hashCode = connectionSocket.recv(1024).decode()[12:]
-    done = False
-    fileBytes = b""
-    #fileBytes = clientSocket.recv(1024)
-    byteData = connectionSocket.recv(1024)[12:]
-    while not done:
-        #byteData = connectionSocket.recv(1024)
-        #byteData = fileBytes
-        if byteData[-5:] == b"<END>":
-            fileBytes += byteData[:-5]
-            print("Test")
-           # print(byteData)
-            #print(fileBytes)
-            done = True
-        else:
-            #byteData = clientSocket.recv(1024)
-            fileBytes += byteData
-            byteData = connectionSocket.recv(1024)
-        
-        
-        print(os.getcwd())
-        #os.chdir("./Database")
-        
+###################################33
     hValidation = hashlib.sha256()
-    hValidation.update(fileBytes)
-    print(hValidation.hexdigest())
-    print(hashCode)
-    print(hValidation.hexdigest() == hashCode)
-    if(hValidation.hexdigest() == hashCode):
-        file = open(fName, "wb")
-        file.write(fileBytes)
-        file.close()
-    else:
-        connectionSocket.close()
-        exit(1)
+    with open(fName, "wb") as f:
+        while True:
+            data = connectionSocket.recv(4096)
+
+            if data == b"<END>":
+            # print("\nDONE\n")
+                break
+
+            f.write(data)
+            hValidation.update(data)
+      #print(b"WRITING DATA "+data)
+            connectionSocket.send("Data recieved".encode(MSGFORMAT))
+    # hashCode = connectionSocket.recv(1024).decode()[12:]
+    # done = False
+    # fileBytes = b""
+    # #fileBytes = clientSocket.recv(1024)
+    # byteData = connectionSocket.recv(1024)[12:]
+    # while not done:
+    #     #byteData = connectionSocket.recv(1024)
+    #     #byteData = fileBytes
+    #     if byteData[-5:] == b"<END>":
+    #         fileBytes += byteData[:-5]
+    #         print("Test")
+    #        # print(byteData)
+    #         #print(fileBytes)
+    #         done = True
+    #     else:
+    #         #byteData = clientSocket.recv(1024)
+    #         fileBytes += byteData
+    #         byteData = connectionSocket.recv(1024)
+        
+        
+    #     print(os.getcwd())
+    #     #os.chdir("./Database")
+        
+    # hValidation = hashlib.sha256()
+    # hValidation.update(fileBytes)
+    # print(hValidation.hexdigest())
+    # print(hashCode)
+    # print(hValidation.hexdigest() == hashCode)
+    # if(hValidation.hexdigest() == hashCode):
+    #     file = open(fName, "wb")
+    #     file.write(fileBytes)
+    #     file.close()
+    # else:
+    #     connectionSocket.close()
+    #     exit(1)
 
 def main():#Main method ran on server start
     path = './Database'
@@ -171,7 +217,7 @@ def main():#Main method ran on server start
         f.close()
         os.chdir('../')
 
-    serverSocket.listen()#server waits for client
+    serverSocket.listen(2)#server waits for client
     print('The server is ready to receive')
     while True:
         connectionSocket, addr = serverSocket.accept()
@@ -184,16 +230,18 @@ def main():#Main method ran on server start
         print(ans)
         if ans == "X":#If the client chooses to download a file
             fileDisp = "<2><LISTFIL>"
-            for filename in fileList:
-                if filename == "passwords.txt":#The client is not allowed to download the password file
+            for filename in fileList:#loop to display all of the files on the server, the passwords.txt file is hidden
+                if filename == "passwords.txt":
                     continue
                 fileDisp += filename + "\n"
-            connectionSocket.send(fileDisp.encode(MSGFORMAT))
+            connectionSocket.send(fileDisp.encode(MSGFORMAT))#
             reqFile = connectionSocket.recv(1024).decode()[12:]
-            findAndSend(reqFile,connectionSocket)
+            findAndSend(reqFile,connectionSocket)#the file the client would like to download is used as a parameter
            # connectionSocket.close()
         elif ans == "Y":
             recvFile(connectionSocket)
+        else:
+            continue
         
 
         
